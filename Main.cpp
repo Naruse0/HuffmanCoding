de41f75864576038ed647547e0dcb20c06b8fea1
Naruse <schoolnarunaru@gmail.com>
2018-09-19 10:07:37 +0900

    Main.cppを追加

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+/**
+ *	@mainpage	Huffman Coding.
+ *	@details	ハフマン符号化を行うアルゴリズムの実装。
+ */
+
+#include	<stdio.h>
+
+/** 
+ *	@brief	エントリポイント
+ */
+int main()
+{
+
+
+
+	// 終了処理
+	printf("Push any key >> ");
+	rewind(stdin);
+	getchar();
+	return 0;
+}
